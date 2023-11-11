df7dabb6e5adfe16616468ed59ab78500761277e
Steve <sohanesian20@gmail.com>
2023-11-11 12:15:18 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.connect();
+
+// app.get('/', (req, res) => {
+//   connection.query('SELECT * FROM users', function (err, rows, fields) {
+//     if (err) throw err
+
+//     res.send(rows)
+//   })
+// })
+
+app.get("/", (req, res) => {
+  // Query to select the first 10 rows from 'properties' table
+  connection.query("SELECT * FROM properties", (err, rows, fields) => {
+    if (err) {
+      // Proper error handling
+      console.error("Error while fetching data: ", err);
+      res.status(500).send("Error while fetching data");
+      return;
+    }
+    // Send the rows as the response
+    res.send(rows);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
