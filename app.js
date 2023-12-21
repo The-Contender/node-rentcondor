ac4ba0f2d36d08b14228ee9049eb566d0e06ce59
Steve <sohanesian20@gmail.com>
2023-12-21 07:34:31 -0800

    Adding compression

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const compression = require("compression"); // Include compression
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors());
+app.use(compression()); // Use compression
 
 const mysql = require("mysql2");
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect();
-
-// app.get('/', (req, res) => {
-//   connection.query('SELECT * FROM users', function (err, rows, fields) {
-//     if (err) throw err
-
-//     res.send(rows)
-//   })
-// })
 
 app.get("/", (req, res) => {
   // Query to select the first 10 rows from 'properties' table
